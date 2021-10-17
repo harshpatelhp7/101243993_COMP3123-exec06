e32580030d66394897be0933ff99d0f4859b5c7e
Harsh <harshpatelhp728@gmail.com>
2021-10-17 14:10:26 -0400

    finish the lab other 4 tasks

--- routes/NoteRoutes.js
+++ routes/NoteRoutes.js
@@ -1,7 +1,6 @@
 //TODO - Create a new Note
 const NoteModel = require('../models/NotesModel');
 const express = require('express');
-const { mongoose } = require('mongoose');
 const app = express();
 
 //http://mongoosejs.com/docs/api.html#document_Document-save
@@ -17,7 +16,7 @@ app.post('/notes', async (req, res) => {
     const N = new NoteModel(req.body) 
     try{    
         await N.save();
-        res.send(N);
+        res.send(`<h1>DATA SAVED IN DATABASE</h1>`)
     }catch(err){
         res.status(500).send(err);
     }
@@ -25,57 +24,68 @@ app.post('/notes', async (req, res) => {
 
 //TODO - Retrieve all Notes
 //http://mongoosejs.com/docs/api.html#find_find
-app.get('/notes', (req, res) => {
+app.get('/notes', async (req, res) => {
     // Validate request
-    if(!req.body) {
+    const notes = await NoteModel.find({});
+    if(notes.length == 0) {
         return res.status(400).send({
-            message: "Note content can not be empty"
+            message: "Database is empty"
         });
     }
     //TODO - Write your code here to returns all note
-    try{
-         res.send(N.find({}))
+    else{try{
+         res.send(notes)
     }catch(err){
         res.status(500).send(err);
-    }
-});
+    }}
+});  
 
 //TODO - Retrieve a single Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyid_findById
-app.get('/notes/:noteId', (req, res) => {
+app.get('/notes/:noteId', async (req, res) => {
+
     // Validate request
-    if(!req.body.content) {
-        return res.status(400).send({
-            message: "Note content can not be empty"
-        });
-    }
     //TODO - Write your code here to return onlt one note using noteid
+    try{
+        const notes = await NoteModel.findById(req.params.noteId);
+        res.send(notes)
+   }catch{
+    return res.status(400).send({
+        message: `No Record associated with ID: ${req.params.noteId}`
+    });
+   }
 });
 
 //TODO - Update a Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyidandupdate_findByIdAndUpdate
-app.put('/notes/:noteId', (req, res) => {
+app.put('/notes/:noteId', async (req, res) => {
     // Validate request
-    if(!req.body.content) {
-        return res.status(400).send({
-            message: "Note content can not be empty"
-        });
-    }
     //TODO - Write your code here to update the note using noteid
-    
-});
-
+    try{
+        const notes = await NoteModel.findByIdAndUpdate(req.params.noteId,req.body);
+        await notes.save();
+        res.send(`Updated Reocrd with Id : ${req.params.noteId}`)
+    }catch{
+        return res.status(400).send({
+            message: `No Record associated with ID: ${req.params.noteId}`
+        });
+       }
+    });
     
 
 //TODO - Delete a Note with noteId
 //http://mongoosejs.com/docs/api.html#findbyidandremove_findByIdAndRemove
-app.delete('/notes/:noteId', (req, res) => {
+app.delete('/notes/:noteId', async (req, res) => {
     // Validate request
-    if(!req.body.content) {
+    try{
+        const notes = await NoteModel.findByIdAndDelete(req.params.noteId);
+        res.send(`Deleted record with ID = ${req.params.noteId}`)
+    }catch{
         return res.status(400).send({
-            message: "Note content can not be empty"
+            message: `No Record associated with ID: ${req.params.noteId}`
         });
-    }
-    //TODO - Write your code here to delete the note using noteid
-});
+       }
+    });
+
+
 module.exports = app
